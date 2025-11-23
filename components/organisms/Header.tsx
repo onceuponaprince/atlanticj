e7220c0629e28d099140f25c1af36ffc680a430e
onceuponaprince <madeitdao@gmail.com>
2025-11-23 21:10:39 +0900

    REMOVED: Logo divbecause of missing asset

--- components/organisms/Header.tsx
+++ components/organisms/Header.tsx
@@ -17,15 +17,15 @@ const navigation = [
     { name: "Contact", href: "/contact" },
 ];
 
-export default function Header({ logo, alt, width, height }: HeaderProps) {
+export default function Header() {
     return (
         <header className="font-primary w-full border-b border-foreground/10 relative">
             <div className="container mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="flex items-center justify-between h-16 md:flex md:justify-between md:items-center md:space-evenly md:w-full">
                     {/* Logo */}
-                    <div className="flex-shrink-0">
+                    {/* <div className="flex-shrink-0">
                         <Logo src={logo} alt={alt} width={width} height={height} />
-                    </div>
+                    </div> */}
 
                     {/* Desktop Navigation */}
                     <nav className="hidden md:flex items-center gap-6">
