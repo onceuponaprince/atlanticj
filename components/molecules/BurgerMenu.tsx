7e6160e5e7b5781b1b8a22757c37aa5a5ae96830
onceuponaprince <madeitdao@gmail.com>
2025-11-24 23:14:49 +0900

    EDITED: Burger Menu Close Button

--- components/molecules/BurgerMenu.tsx
+++ components/molecules/BurgerMenu.tsx
@@ -37,7 +37,7 @@ export default function BurgerMenu({ children }: BurgerMenuProps) {
       {/* Mobile Menu */}
       {isOpen && (
         <div className="absolute top-0 right-0 h-screen w-screen m-0 p-0 bg-transparent z-50 md:hidden animate-in fade-in slide-in-from-top-500 ease-in-out">
-          <BurgerButton handleClick={handleClick} isOpen={isOpen} className="absolute bg-transparent top-[10px] right-[58.5px] slide-out-to-top-500 ease-in-out duration-300 animate-out justify-self-center justify-center items-center text-center align-middle"/>
+          <BurgerButton handleClick={handleClick} isOpen={isOpen} className="absolute bg-transparent top-2.5 right-14.5 slide-out-to-top-500 ease-in-out duration-300 animate-out justify-self-center justify-center items-center text-center align-middle"/>
           <div className="flex flex-col bg-background gap-5 mx-auto h-full w-full justify-self-center justify-center items-center text-center align-middle justify-self-center justify-center items-center text-center align-middle">
             {children}
           </div>
